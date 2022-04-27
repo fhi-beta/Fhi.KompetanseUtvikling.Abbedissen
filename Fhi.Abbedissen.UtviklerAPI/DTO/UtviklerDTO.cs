d93aaff7ffca606c8dd18f7c65422c15c1764344
Odd Andreas Hartvigsen <OddAndreas.Hartvigsen@fhi.no>
2022-04-27 13:11:27 +0200

    Lagt til utviklerkontroller

---
+++ Fhi.Abbedissen.UtviklerAPI/DTO/UtviklerDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Fhi.Abbedissen.UtviklerAPI.DTO
+{
+    public class UtviklerDTO
+    {
+        public int Id { get; set; }
+        public string Fornavn { get; set; }
+        public string Etternavn { get; set; }
+    }
+}
---
+++ Fhi.Abbedissen.UtviklerAPI/Services/UtviklerService.cs
@@ -0,0 +1,36 @@
+﻿using Fhi.Abbedissen.UtviklerAPI.DTO;
+using Fhi.Abbedissen.UtviklerAPI.Model;
+
+namespace Fhi.Abbedissen.UtviklerAPI.Services
+{
+    public class UtviklerService : IUtviklerService
+    {
+        private List<Utvikler> _Utviklere;
+
+        public UtviklerService()
+        {
+            _Utviklere = new List<Utvikler>();
+            _Utviklere.Add(new Utvikler() { Id = 1, Fornavn = "Per", Etternavn = "Askeladd", Opprettet=DateTime.Now });
+            _Utviklere.Add(new Utvikler() { Id = 2, Fornavn = "Pål", Etternavn = "Askeladd", Opprettet = DateTime.Now });
+            _Utviklere.Add(new Utvikler() { Id = 3, Fornavn = "Espen", Etternavn = "Askeladd", Opprettet = DateTime.Now });
+            _Utviklere.Add(new Utvikler() { Id = 4, Fornavn = "Gunnar", Etternavn = "Askeladd", Opprettet = DateTime.Now });
+        }
+
+        public Utvikler? HentUtvikler(int id)
+        {
+            var resultat = _Utviklere.FirstOrDefault(x => x.Id == id);
+            if (resultat == null) return null;
+            return resultat;
+        }
+
+        public IEnumerable<Utvikler> HentUtviklere()
+        {
+            return _Utviklere;
+        }
+
+        public void LeggTilUtvikler(Utvikler utvikler)
+        {
+            _Utviklere.Add(utvikler);
+        }
+    }
+}
