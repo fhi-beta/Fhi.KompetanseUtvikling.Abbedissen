23e88a5507d3fea318b8c98ca2c10c162f4d5e7f
Torbjørn Gjerde <Torbjorn.Gjerde@fhi.no>
2022-05-25 14:10:57 +0200

    Kompetanse, rating mot utvikler model scaffolding

---
+++ Fhi.Abbedissen.KompetanseAPI/Model/Rating.cs
@@ -0,0 +1,10 @@
+﻿namespace Fhi.Abbedissen.KompetanseAPI.Model
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public string? Navn { get; set; }
+        public string? Beskrivelse { get; set; }
+        public DateTime OpprettetDato { get; set; }
+    }
+}
