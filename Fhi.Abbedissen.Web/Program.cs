ff2f0a484a72d9c69dbd9fd50d4fd15e27bfe92e
janarild.eileng <janarild.eileng@fhi.no>
2022-05-25 10:27:47 +0200

    Lagt til Swagger i Web prosjekt

--- Fhi.Abbedissen.Web/Program.cs
+++ Fhi.Abbedissen.Web/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 builder.Services.AddControllersWithViews();
 
@@ -11,7 +14,15 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+   
 }
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
