b4df700660290592780cc5cb00326e5c0437186e
Elin Skaflestad <Elin.Skaflestad@fhi.no>
2022-05-25 10:13:45 +0200

    legg til kompetanseservice

---
+++ Fhi.Abbedissen.KompetanseAPI/Services/IKompetanseService.cs
@@ -0,0 +1,11 @@
+﻿using Fhi.Abbedissen.KompetanseAPI.Model; 
+
+namespace Fhi.Abbedissen.KompetanseAPI.Services
+{
+    public interface IKompetanseService
+    {
+        IEnumerable<Kompetanse> HentKompetanse();
+        Kompetanse HentKompetanse(int id);
+        void LeggTilKompetanse(Kompetanse kompetanse);
+    }
+}
